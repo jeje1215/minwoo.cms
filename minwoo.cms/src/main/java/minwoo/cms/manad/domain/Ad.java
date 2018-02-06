f63db0223f5f9a990d0896afd88bfe114cb9165f
tjoeungr-s <tjoeungr-s@tjoeun>
2018-02-06 17:48:09 +0900

    유나광고

--- minwoo.cms/src/main/java/minwoo/cms/manad/domain/Ad.java
+++ minwoo.cms/src/main/java/minwoo/cms/manad/domain/Ad.java
@@ -10,5 +10,42 @@ public class Ad {
 	private int adPrice; //광고 금액
 	private String userName; //광고관리자이름
 	
+	public int getAdNo() {
+		return adNo;
+	}
+	public void setAdNo(int adNo) {
+		this.adNo = adNo;
+	}
+	public String getAdName() {
+		return adName;
+	}
+	public void setAdName(String adName) {
+		this.adName = adName;
+	}
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getFinshDate() {
+		return finshDate;
+	}
+	public void setFinshDate(Date finshDate) {
+		this.finshDate = finshDate;
+	}
+	public int getAdPrice() {
+		return adPrice;
+	}
+	public void setAdPrice(int adPrice) {
+		this.adPrice = adPrice;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
 
 }

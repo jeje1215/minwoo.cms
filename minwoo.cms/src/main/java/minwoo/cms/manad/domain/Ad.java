0afa6d362ed09929cada1fa72d8140207bad4a7c
tjoeungr-s <tjoeungr-s@tjoeun>
2018-02-06 17:07:12 +0900

    유나

--- minwoo.cms/src/main/java/minwoo/cms/manad/domain/Ad.java
+++ minwoo.cms/src/main/java/minwoo/cms/manad/domain/Ad.java
@@ -1,9 +1,14 @@
 package minwoo.cms.manad.domain;
 
+import java.sql.Date;
+
 public class Ad {
-	private int adNo;
-	private String userName;
-	private Date startDate;
-	private Date finshDate;
+	private int adNo; //광고번호
+	private String adName; //광고이름
+	private Date startDate; //광고 시작일
+	private Date finshDate; //광고 종료일
+	private int adPrice; //광고 금액
+	private String userName; //광고관리자이름
+	
 
 }

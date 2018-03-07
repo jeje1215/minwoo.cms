acea5c53a49a7abb18f882bb93b736cdd868ba8a
tj <tj@tj-PC>
2018-03-07 09:54:12 +0900

    수정수정

--- minwoo.cms/src/main/java/minwoo/cms/board/dao/BoardDao.java
+++ minwoo.cms/src/main/java/minwoo/cms/board/dao/BoardDao.java
@@ -7,10 +7,9 @@ import minwoo.cms.board.domain.BoardVo;
 public interface BoardDao {
    List<BoardVo> getBoardLists(); //게시글 목록
    int addBoard(BoardVo boardVo); //게시글 등록
-   BoardVo oneListBoard(int usbId);// 게시글 하나가져오기
+   BoardVo oneListBoard(int usbId); // 게시글 하나가져오기
    public BoardVo readBoard(int usbId); //게시글 상세보기
-   int updateBoard(BoardVo boardVo);//게시글 수정
-   int deleteBoard(int usbId);//게시글 삭제
-
-   void updateCnt(int usbId);
+   int updateBoard(BoardVo boardVo); //게시글 수정
+   int deleteBoard(int usbId); //게시글 삭제
+   void updateCnt(int usbId); //게시글 조회수
 }
